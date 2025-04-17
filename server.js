9f2e8c3d3b1f8224c5eddef66f6a7ce946d1090f
DanDev <162236581+Danrp027@users.noreply.github.com>
2025-04-17 18:41:21 +0000

    arquivos

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-
 const express = require('express');
 const path = require('path');
-const app = express();
-const estoque = require('./estoque');
-const db = require('./db');
+const sqlite = require('sqlite3').verbose();
+const nodemailer = require('nodemailer');
 
+const app = express();
+
+const db = new sqlite.Database("LogCore.sqlite");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/estoque', estoque);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(3000, console.log("Rodando... http://localhost:3000"));
+
